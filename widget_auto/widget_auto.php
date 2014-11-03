11b166ab0d5d4776ed04c7d174da4bb3ea1fc574
Vladimir Shestakov <boolive@yandex.ru>
2014-11-03 19:07:38 +0500

    added part

---
+++ widget_auto/widget_auto.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * widget_auto
+ * @aurhor Vladimir Shestakov
+ * @version 1.0
+ */
+namespace boolive\basic\widget_auto;
+
+use boolive\basic\widget\widget;
+use boolive\core\request\Request;
+
+class widget_auto extends widget
+{
+    function show($v, Request $request)
+    {
+        $v['views'] = $this->views->start($request);
+        return parent::show($v, $request);
+    }
+} 
---
+++ widget_auto/widget_auto.tpl
@@ -0,0 +1,3 @@
+<div class="widget widget_auto">99
+    <?= $v['views']->string(); ?>
+</div>
